1d0d51c6154c99591212537650d3a21a0bc4a348
Michelle <newbrainspace@gmail.com>
2024-08-28 09:33:31 +1000

    began adding docstrings to bench file, fixed an issue with whitening matrix edge cases, fixed an indexing issue. benchmarking now works as intended

--- kilosort/bench.py
+++ kilosort/bench.py
@@ -11,7 +11,7 @@ def get_drift_matrix(ops, dshift):
     """
 
     # first, interpolate drifts to every channel
-    yblk = ops['yblk']
+    yblk = ops['yblk'] #1-D array; dshift should be N-D array, interpolated along last axis
     finterp = interp1d(yblk, dshift, fill_value="extrapolate", kind = 'linear')
     shifts = finterp(ops['probe']['yc'])
 
@@ -99,7 +99,7 @@ def load_transform(filename, ibatch, ops, fwav=None, Wrot = None, dshift = None)
         if dshift is not None:
             if isinstance(iKxx, np.ndarray):
                 ops['iKxx'] = torch.from_numpy(iKxx).to(dev)
-            M = get_drift_matrix(ops, dshift[ibatch])
+            M = preprocessing.get_drift_matrix(ops, dshift[ibatch])
             X = (M @ Wrot) @ X
         else:
             X = Wrot @ X
@@ -107,6 +107,21 @@ def load_transform(filename, ibatch, ops, fwav=None, Wrot = None, dshift = None)
     return X
 
 def avg_wav(filename, Wsub, nn, ops, ibatch, st_i, clu, Nfilt):
+    """
+    updates the average waveforms for clusters
+    Args:  
+        filename (str): the path to the binary recording
+        Wsub ([[float]]): A running average for each waveform
+        nn ([int]): the number of spikes currently contributing to each average waveform
+        ops (dict): operating parameter dictionary. needs nt, batch_size, wPCA, fwav, Wrot, dshift
+        ibatch (int): batch index
+        st_i ([int]): spike times adjusted for offset
+        clu ([int]): cluster labels for identified spikes
+        Nfilt (int): the number of clusters
+    Returns:
+        Wsub ([[float]]): updated running average for each waveform
+        nn ([int]): updated number of spikes currently contributing to each average waveform
+    """
     nt = ops['nt']
     NT = ops['batch_size']
     if isinstance(ops['wPCA'], np.ndarray):
@@ -131,9 +146,20 @@ def avg_wav(filename, Wsub, nn, ops, ibatch, st_i, clu, Nfilt):
     return Wsub, nn
 
 def clu_ypos(filename, ops, st_i, clu):
+    """
+    FUNCTION DESCRIPTION
+    Args: 
+        filename (str): 
+        ops (dict): operational parameters. nwaves, Nchan, and Nbatches must be defined.
+        st_i ([int]): list of spike times
+        clu ([int]): cluster labels for identified spikes
+    Returns: 
+        yclu (TODO datatype): y location of each identified cluster
+        Wsub (TODO datatype idc): average waveform for each identified cluster
+    """
     Nfilt = clu.max()+1
-    Wsub = torch.zeros((Nfilt, ops['nwaves'], ops['Nchan']), device = dev)
-    nn   = torch.zeros((Nfilt, ), device = dev)
+    Wsub = torch.zeros((Nfilt, ops['nwaves'], ops['Nchan']), device = dev) #accumulates average waveform for each cluster
+    nn   = torch.zeros((Nfilt, ), device = dev) #tracks number of spikes contributing to each cluster's waveform
 
     for ibatch in range(0, ops['Nbatches'], 10):
         Wsub, nn = avg_wav(filename, Wsub, nn, ops, ibatch, st_i, clu, Nfilt)
@@ -169,6 +195,26 @@ def nmatch(ss0, ss, dt=6):
     return n0, is_matched, is_matched0
 
 def match_neuron(kk, clu, yclu, st_i, clu0, yclu0, st0_i, n_check=20, dt=6):
+    """
+    FUNCTION DESCRIPTION
+    Args: 
+        kk
+        clu
+        yclu
+        st_i
+        clu0
+        yclu0
+        st0_i
+        n_check
+        dt=6
+    Returns:
+        fmax
+        fmiss
+        fpos
+        best_ind
+        matched_all
+        top_inds
+    """
     ss = st_i[clu==kk]
     isort = np.argsort(np.abs(yclu[kk] - yclu0))
     fmax = 0
@@ -201,6 +247,24 @@ def match_neuron(kk, clu, yclu, st_i, clu0, yclu0, st0_i, n_check=20, dt=6):
     return fmax, miss, fpos, best_ind, matched_all, top_inds
 
 def compare_recordings(st_gt, clu_gt, yclu_gt, st_new, clu_new, yclu_new):
+    """
+    compares two recordings
+    Args:
+        st_gt ([int]): the ground truth spike times
+        clu_gt ([int]): the cluster labels for each identified spike
+        yclu_gt ([float32]): 
+        st_new ([int]): the spike times output by the sorter
+        clu_new ([int]): the cluster labels for each identified spike
+        yclu_new ([float32]): 
+    Returns: 
+        fmax ([float64])
+        fmiss ([float64])
+        fpos ([float64])
+        best_ind ([int])
+        matched_all ([[float64]])
+        top_inds ([[int]])
+    """
+    
     NN = len(yclu_gt)
 
     n_check = 20
@@ -218,6 +282,22 @@ def compare_recordings(st_gt, clu_gt, yclu_gt, st_new, clu_new, yclu_new):
     return fmax, fmiss, fpos, best_ind, matched_all, top_inds
 
 def load_GT(filename, ops, gt_path, toff = 20, nmax = 600):
+    """
+    Loads ground truth to format usable by compare_recordings
+    Args: 
+        filename (str): the path to the raw binary file (spike recording)
+        ops (dict): options dict, typically loaded from ops.npy in kilosort output. requires 'nwaves': 6 to be added
+        gt_path (str): the path to the ground truth file (typical name "sim.imec0.ap_params.npz")
+        toff (int=20): time offset (samples)
+        nmax (int=600): maximum number of neurons to consider
+    Returns:
+        st_gt ([int64]): ground truth spike times
+        clu_gt ([int64]): ground truth cluster labels for each spike
+        yclu_gt
+        mu_gt
+        Wsub
+        nsp
+    """
     #gt_path = os.path.join(ops['data_folder'] , "sim.imec0.ap_params.npz")
     dd = np.load(gt_path, allow_pickle = True)
 
@@ -238,9 +318,14 @@ def load_GT(filename, ops, gt_path, toff = 20, nmax = 600):
     return st_gt, clu_gt, yclu_gt, mu_gt, Wsub, nsp
 
 
-def convert_ks_output(ops, st, clu, toff = 20):
-    st = st[:,0].astype('int64')        
-    yclu, Wsub    = clu_ypos(ops, st-toff, clu)
+def convert_ks_output(filename, ops, st, clu, toff = 20):
+    """
+    note that kilosort output ops doesn't include 'nwaves', which is necessary
+    to run this function. nwaves should be 6. No idea why.
+    Pretty sure st is spike times but they indexed it stupid lmao
+    """
+    st = st[:].astype('int64')        
+    yclu, Wsub    = clu_ypos(filename, ops, st-toff, clu)
 
     return st, clu, yclu, Wsub
 
